27a8d7db929011c3d4e0f0f6f236874366370a7c
wnmwi <383929371@qq.com>
2019-10-15 23:00:03 +0800

    第一次提交

---
+++ NetMeetingExample/FormMeeting.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace NetMeetingExample
+{
+    partial class FormMeeting
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBoxMessage = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBoxAddress = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.groupBoxRoom = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBoxRoom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(12, 12);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(100, 25);
+            this.buttonLogin.TabIndex = 0;
+            this.buttonLogin.Text = "进入会议室";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Location = new System.Drawing.Point(141, 12);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(100, 25);
+            this.buttonLogout.TabIndex = 1;
+            this.buttonLogout.Text = "离开会议室";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBoxMessage);
+            this.groupBox1.Location = new System.Drawing.Point(6, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(222, 209);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "相关信息";
+            // 
+            // listBoxMessage
+            // 
+            this.listBoxMessage.FormattingEnabled = true;
+            this.listBoxMessage.ItemHeight = 17;
+            this.listBoxMessage.Location = new System.Drawing.Point(6, 22);
+            this.listBoxMessage.Name = "listBoxMessage";
+            this.listBoxMessage.Size = new System.Drawing.Size(210, 174);
+            this.listBoxMessage.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBoxAddress);
+            this.groupBox2.Location = new System.Drawing.Point(234, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(219, 209);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "会议室现有成员";
+            // 
+            // listBoxAddress
+            // 
+            this.listBoxAddress.FormattingEnabled = true;
+            this.listBoxAddress.ItemHeight = 17;
+            this.listBoxAddress.Location = new System.Drawing.Point(6, 22);
+            this.listBoxAddress.Name = "listBoxAddress";
+            this.listBoxAddress.Size = new System.Drawing.Size(207, 174);
+            this.listBoxAddress.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBoxMessage);
+            this.groupBox3.Location = new System.Drawing.Point(12, 287);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(459, 62);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(6, 22);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(447, 23);
+            this.textBoxMessage.TabIndex = 0;
+            this.textBoxMessage.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // groupBoxRoom
+            // 
+            this.groupBoxRoom.Controls.Add(this.groupBox1);
+            this.groupBoxRoom.Controls.Add(this.groupBox2);
+            this.groupBoxRoom.Location = new System.Drawing.Point(12, 44);
+            this.groupBoxRoom.Name = "groupBoxRoom";
+            this.groupBoxRoom.Size = new System.Drawing.Size(459, 237);
+            this.groupBoxRoom.TabIndex = 5;
+            this.groupBoxRoom.TabStop = false;
+            this.groupBoxRoom.Text = "会议室";
+            // 
+            // FormMeeting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.groupBoxRoom);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.buttonLogout);
+            this.Controls.Add(this.buttonLogin);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FormMeeting";
+            this.Text = "Internet网络会议";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMeeting_FormClosing);
+            this.Load += new System.EventHandler(this.FormMeeting_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBoxRoom.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonLogout;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBoxMessage;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBoxAddress;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.GroupBox groupBoxRoom;
+    }
+}
+
---
+++ NetMeetingExample/FormMeeting.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetMeetingExample
+{
+    public partial class FormMeeting : Form
+    {
+        private enum ListBoxOperation { AddItem, RemoveItem };
+        private delegate void SetListBoxItemCallback(
+        ListBox listbox, string text, ListBoxOperation operation);
+        SetListBoxItemCallback listBoxCallback;
+        //使用的IP 地址
+        private IPAddress ip = IPAddress.Parse("224.100.0.1");
+        //使用的接收端口号
+        private int port = 8001;
+        private UdpClient udpClient;
+        public FormMeeting()
+        {
+            InitializeComponent();
+            listBoxCallback = new SetListBoxItemCallback(SetListBoxItem);
+        }
+        private void SetListBoxItem(ListBox listbox, string text, ListBoxOperation operation)
+        {
+            if (listbox.InvokeRequired == true)
+            {
+                this.Invoke(listBoxCallback, listbox, text, operation);
+            }
+            else
+            {
+                if (operation == ListBoxOperation.AddItem)
+                {
+                    if (listbox == listBoxAddress)
+                    {
+                        if (listbox.Items.Contains(text) == false)
+                        {
+                            listbox.Items.Add(text);
+                        }
+                    }
+                    else
+                    {
+                        listbox.Items.Add(text);
+                    }
+                    listbox.SelectedIndex = listbox.Items.Count - 1;
+                    listbox.ClearSelected();
+                }
+                else if (operation == ListBoxOperation.RemoveItem)
+                {
+                    listbox.Items.Remove(text);
+                }
+            }
+        }
+        private void SendToAll(string sendString)
+        {
+            UdpClient myUdpClient = new UdpClient();
+            myUdpClient.Ttl = 50;
+            //允许发送和接收广播数据报
+            myUdpClient.EnableBroadcast = true;
+            //必须使用组播地址范围内的地址
+            IPEndPoint iep = new IPEndPoint(ip, port);
+            //将发送内容转换为字节数组
+            byte[] bytes = Encoding.UTF8.GetBytes(sendString);
+            try
+            {
+                //向子网发送信息
+                myUdpClient.Send(bytes, bytes.Length, iep);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "发送失败");
+            }
+            finally
+            {
+                myUdpClient.Close();
+            }
+        }
+
+        private void FormMeeting_Load(object sender, EventArgs e)
+        {
+            listBoxMessage.HorizontalScrollbar = true;
+            buttonLogin.Enabled = true;
+            buttonLogout.Enabled = false;
+            groupBoxRoom.Enabled = false;
+        }
+
+        /// <summary>
+        /// 接收线程
+        /// </summary>
+        private void ReceiveMessage()
+        {
+            udpClient = new UdpClient(port);
+            //必须使用组播地址范围内的地址
+            udpClient.JoinMulticastGroup(ip);
+            udpClient.Ttl = 50;
+            IPEndPoint remote = null;
+            while (true)
+            {
+                try
+                {
+                    //关闭udpClient 时此句会产生异常
+                    byte[] bytes = udpClient.Receive(ref remote);
+                    string str = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+                    string[] splitString = str.Split(',');
+                    int s = splitString[0].Length;
+                    switch (splitString[0])
+                    {
+                        case "Login": //进入会议室
+                            SetListBoxItem(listBoxMessage, string.Format("[{0}]进入。", remote.Address), ListBoxOperation.AddItem);
+                            string userListString = "List," + remote.Address.ToString();
+                            for (int i = 0; i < listBoxAddress.Items.Count; i++)
+                            {
+                                userListString += "," + listBoxAddress.Items[i].ToString();
+                            }
+                            SendToAll(userListString);
+                            break;
+                        case "List": //参加会议人员名单
+                            for (int i = 1; i < splitString.Length; i++)
+                            {
+                                SetListBoxItem(listBoxAddress, splitString[i], ListBoxOperation.AddItem);
+                            }
+                            break;
+                        case "Message": //发言内容
+                            SetListBoxItem(listBoxMessage,
+                            string.Format("[{0}]说：{1}", remote.Address, str.Substring(8)),
+                            ListBoxOperation.AddItem);
+                            break;
+                        case "Logout": //退出会议室
+                            SetListBoxItem(listBoxMessage, string.Format("[{0}]退出。", remote.Address),ListBoxOperation.AddItem);
+                            SetListBoxItem(listBoxAddress,
+                            remote.Address.ToString(), ListBoxOperation.RemoveItem);
+                            break;
+                    }
+                }
+                catch
+                {
+                    //退出循环，结束线程
+                    break;
+                }
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                if (textBoxMessage.Text.Trim().Length > 0)
+                {
+                    SendToAll("Message," + textBoxMessage.Text);
+                    textBoxMessage.Text = "";
+                }
+            }
+        }
+
+        private void FormMeeting_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (buttonLogout.Enabled == true)
+            {
+                MessageBox.Show("请先离开会议室，然后再退出！", "",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                //不关闭窗体
+                e.Cancel = true;
+            }
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            Thread myThread = new Thread(new ThreadStart(ReceiveMessage));
+            myThread.Start();
+            //等待接收线程准备完毕
+            Thread.Sleep(1000);
+            SendToAll("Login");
+            buttonLogin.Enabled = false;
+            buttonLogout.Enabled = true;
+            groupBoxRoom.Enabled = true;
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            SendToAll("Logout");
+            //等待接收线程处理完毕
+            Thread.Sleep(1000);
+            //结束接收线程
+            udpClient.Close();
+            buttonLogin.Enabled = true;
+            buttonLogout.Enabled = false;
+            groupBoxRoom.Enabled = false;
+            Cursor.Current = Cursors.Default;
+        }
+    }
+}
